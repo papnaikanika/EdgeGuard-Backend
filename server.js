98572efde611927541df50435f8611d41d400aa5
Kanika Papnai <papnaikanika@gmail.com>
2026-09-11 11:08:39 +0530

    Use Render assigned port

--- server.js
+++ server.js
@@ -1125,8 +1125,7 @@ app.use(
 //                  START SERVER
 // =================================================
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 app.listen(
     PORT,
     () => {
